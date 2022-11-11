8561e5b759bee932ea8c806dd4920fd96bf73716
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2022-11-11 09:35:57 +0530

    reversing string in modern way:))

--- Generating and Manipulating/main.cc
+++ Generating and Manipulating/main.cc
@@ -13,15 +13,26 @@ void print(const std::vector<ZT> &v)
 int main(void)
 {
     std::vector<int> n(100);
-    std::fill(std::begin(n), std::end(n), 5);
-    print<int>(n);
-    std::fill_n(std::begin(n), n.size() / 2, 0);
-    print<int>(n);
+    // std::fill(std::begin(n), std::end(n), 5);
+    // print<int>(n);
+    // std::fill_n(std::begin(n), n.size() / 2, 0);
+    // print<int>(n);
     std::generate(std::begin(n), std::end(n), []()
-                  { return rand() % 100; });
-    print<int>(n);
+                  { return rand() % 10; });
+    // print<int>(n);
 
-    std::sort(std::begin(n), std::find(std::begin(n), std::end(n), 1));
-    print<int>(n);
+    // std::sort(std::begin(n), std::find(std::begin(n), std::end(n), 1));
+    // print<int>(n);
+    //     std::sort(std::begin(n), std::end(n));
+    //     std::vector<int> n_copy(n.size());
+    //     std::unique_copy(std::begin(n), std::end(n), std::begin(n_copy));
+    //    auto returnValues= std::unique(std::begin(n),std::end(n));
+    //     print<int>(n);
+    //     print<int>(n_copy);
+    std::string text{"hello, world!"};
+    std::cout << text << "\n";
+    std::reverse(std::begin(text), std::end(text));
+    std::cout << text << "\n";
+
     return 0;
 }
